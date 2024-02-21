7fc4cd03bf50dd79afbd76db3cfb2b711c62fead
Dustin Ruetz <26169612+dustin-ruetz@users.noreply.github.com>
2024-02-21 15:27:26 -0500

    Creating and Initializing the Repository (#1)
    * feat: replace 'xyz' with 'onepassword' and ensure that GitHub repo URLs are accurate (i.e. github.com/1Password/pulumi-provider-onepassword)
    
    * feat: files that were renamed by 'make prepare' command
    
    * feat(docs): initial updates for README-PROVIDER.md
    
    * fix: correct 'ORG' value in Makefile
    
    * fix: update 'PluginDownloadURL' value to use 'github://' URL protocol
    
    * feat: rename repository to 'pulumi-onepassword'
    
    * fix: change name of Python package to 'pulumi-onepassword'
    
    * fix: re-add missing comma
    
    * feat: use Go 1.21

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
-PROJECT_NAME := xyz Package
+PROJECT_NAME := onepassword Package
 
 SHELL            := /bin/bash
-PACK             := xyz
-ORG              := pulumi
+PACK             := onepassword
+ORG              := 1Password
 PROJECT          := github.com/${ORG}/pulumi-${PACK}
 NODE_MODULE_NAME := @pulumi/${PACK}
 TF_NAME          := ${PACK}
@@ -29,13 +29,13 @@ prepare::
 	mv "provider/cmd/pulumi-resource-x${EMPTY_TO_AVOID_SED}yz" provider/cmd/pulumi-resource-${NAME}
 
 	if [[ "${OS}" != "Darwin" ]]; then \
-		sed -i 's,github.com/pulumi/pulumi-xyz,${REPOSITORY},g' provider/go.mod; \
+		sed -i 's,github.com/pulumi/pulumi-onepassword,${REPOSITORY},g' provider/go.mod; \
 		find ./ ! -path './.git/*' -type f -exec sed -i 's/[x]yz/${NAME}/g' {} \; &> /dev/null; \
 	fi
 
 	# In MacOS the -i parameter needs an empty string to execute in place.
 	if [[ "${OS}" == "Darwin" ]]; then \
-		sed -i '' 's,github.com/pulumi/pulumi-xyz,${REPOSITORY},g' provider/go.mod; \
+		sed -i '' 's,github.com/pulumi/pulumi-onepassword,${REPOSITORY},g' provider/go.mod; \
 		find ./ ! -path './.git/*' -type f -exec sed -i '' 's/[x]yz/${NAME}/g' {} \; &> /dev/null; \
 	fi
 
--- provider/resources.go
+++ provider/resources.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package xyz
+package onepassword
 
 import (
 	"fmt"
@@ -28,18 +28,18 @@ import (
 	"github.com/pulumi/pulumi/sdk/v3/go/common/resource"
 
 	// Replace this provider with the provider you are bridging.
-	xyz "github.com/iwahbe/terraform-provider-xyz/provider"
+	onepassword "github.com/1Password/terraform-provider-onepassword/onepassword"
 
-	"github.com/pulumi/pulumi-xyz/provider/pkg/version"
+	"github.com/1Password/pulumi-onepassword/provider/pkg/version"
 )
 
 // all of the token components used below.
 const (
 	// This variable controls the default name of the package in the package
 	// registries for nodejs and python:
-	mainPkg = "xyz"
+	mainPkg = "onepassword"
 	// modules:
-	mainMod = "index" // the xyz module
+	mainMod = "index" // the onepassword module
 )
 
 // preConfigureCallback is called before the providerConfigure function of the underlying provider.
@@ -50,7 +50,7 @@ func preConfigureCallback(resource.PropertyMap, shim.ResourceConfig) error {
 	return nil
 }
 
-//go:embed cmd/pulumi-resource-xyz/bridge-metadata.json
+//go:embed cmd/pulumi-resource-onepassword/bridge-metadata.json
 var metadata []byte
 
 // Provider returns additional overlaid schema and metadata associated with the provider..
@@ -58,8 +58,8 @@ func Provider() tfbridge.ProviderInfo {
 	// Create a Pulumi provider mapping
 	prov := tfbridge.ProviderInfo{
 		// Instantiate the Terraform provider
-		P:    shimv2.NewProvider(xyz.New(version.Version)()),
-		Name: "xyz",
+		P:    shimv2.NewProvider(onepassword.New(version.Version)()),
+		Name: "onepassword",
 		// DisplayName is a way to be able to change the casing of the provider
 		// name when being displayed on the Pulumi registry
 		DisplayName: "",
@@ -76,19 +76,19 @@ func Provider() tfbridge.ProviderInfo {
 		LogoURL: "",
 		// PluginDownloadURL is an optional URL used to download the Provider
 		// for use in Pulumi programs
-		// e.g https://github.com/org/pulumi-provider-name/releases/
-		PluginDownloadURL: "",
-		Description:       "A Pulumi package for creating and managing xyz cloud resources.",
+		// e.g github://api.github.com/org/pulumi-provider-name
+		PluginDownloadURL: "github://api.github.com/1Password/pulumi-onepassword",
+		Description:       "Use the 1Password Pulumi provider to access and manage items in your 1Password vaults.",
 		// category/cloud tag helps with categorizing the package in the Pulumi Registry.
 		// For all available categories, see `Keywords` in
 		// https://www.pulumi.com/docs/guides/pulumi-packages/schema/#package.
-		Keywords:   []string{"pulumi", "xyz", "category/cloud"},
+		Keywords:   []string{"pulumi", "onepassword", "1Password", "category/cloud"},
 		License:    "Apache-2.0",
 		Homepage:   "https://www.pulumi.com",
-		Repository: "https://github.com/pulumi/pulumi-xyz",
+		Repository: "https://github.com/1Password/pulumi-onepassword",
 		// The GitHub Org for the provider - defaults to `terraform-providers`. Note that this
 		// should match the TF provider module's require directive, not any replace directives.
-		GitHubOrg:    "",
+		GitHubOrg:    "1Password",
 		MetadataInfo: tfbridge.NewProviderMetadata(metadata),
 		Config:       map[string]*tfbridge.SchemaInfo{
 			// Add any required configuration here, or remove the example below if
@@ -160,7 +160,7 @@ func Provider() tfbridge.ProviderInfo {
 	// tokens, and apply auto aliasing for full backwards compatibility.  For more
 	// information, please reference:
 	// https://pkg.go.dev/github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge#ProviderInfo.ComputeTokens
-	prov.MustComputeTokens(tokens.SingleModule("xyz_", mainMod,
+	prov.MustComputeTokens(tokens.SingleModule("onepassword_", mainMod,
 		tokens.MakeStandard(mainPkg)))
 	prov.MustApplyAutoAliases()
 	prov.SetAutonaming(255, "-")
