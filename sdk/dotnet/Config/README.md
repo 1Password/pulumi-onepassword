7f5fb6350f1d414dc104f58482fb8e1f02cb91ec
Dustin Ruetz <dustin.ruetz@agilebits.com>
2024-03-08 20:12:15 -0500

    feat: build out language SDKs

---
+++ sdk/dotnet/Config/README.md
@@ -0,0 +1,1 @@
+Use the 1Password Pulumi provider to access and manage items in your 1Password vaults.
---
+++ sdk/go/onepassword/doc.go
@@ -0,0 +1,2 @@
+// Use the 1Password Pulumi provider to access and manage items in your 1Password vaults.
+package onepassword
