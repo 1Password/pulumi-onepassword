29272cf29ca995316918e544d362157a64d84c68
Eddy Filip <eddy.filip@agilebits.com>
2024-04-03 13:34:54 +0200

    Add E2E testing for NodeJS example

--- examples/examples_nodejs_test.go
+++ examples/examples_nodejs_test.go
@@ -4,6 +4,7 @@
 package examples
 
 import (
+	"path/filepath"
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v3/testing/integration"
@@ -13,9 +14,17 @@ func getJSBaseOptions(t *testing.T) integration.ProgramTestOptions {
 	base := getBaseOptions()
 	baseJS := base.With(integration.ProgramTestOptions{
 		Dependencies: []string{
-			"@pulumi/foo",
+			"@pulumi/onepassword",
 		},
 	})
 
 	return baseJS
+}
+
+func TestProviderTs(t *testing.T) {
+    test := getJSBaseOptions(t).
+        With(integration.ProgramTestOptions{
+            Dir: filepath.Join(getCwd(t), "provider-example", "ts"),
+        })
+    integration.ProgramTest(t, &test)
 }
