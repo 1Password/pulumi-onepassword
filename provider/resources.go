8593f39516f483c9bfecf49835123deb50b27f7c
Dustin Ruetz <dustin.ruetz@agilebits.com>
2024-04-24 11:50:48 -0400

    feat(provider/resources): specify Python package name as 'pulumi-onepassword'

--- provider/resources.go
+++ provider/resources.go
@@ -150,6 +150,7 @@ func Provider() tfbridge.ProviderInfo {
 			//Overlay: &tfbridge.OverlayInfo{},
 		},
 		Python: &tfbridge.PythonInfo{
+			PackageName: "pulumi-onepassword",
 			// List any Python dependencies and their version ranges
 			Requires: map[string]string{
 				"pulumi": ">=3.0.0,<4.0.0",
