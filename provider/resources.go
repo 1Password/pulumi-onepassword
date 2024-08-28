51b4f4ef3b68cfc76f651e0dda0f4b21e13ed964
Eddy Filip <eddy.filip@agilebits.com>
2024-08-28 19:54:53 +0300

    Add logo URL

--- provider/resources.go
+++ provider/resources.go
@@ -72,7 +72,7 @@ func Provider() tfbridge.ProviderInfo {
 		//
 		// You may host a logo on a domain you control or add an SVG logo for your package
 		// in your repository and use the raw content URL for that file as your logo URL.
-		LogoURL: "",
+		LogoURL: "https://1password.com/logo-images/1password-logo-dark@2x.png",
 		// PluginDownloadURL is an optional URL used to download the Provider
 		// for use in Pulumi programs
 		// e.g github://api.github.com/org/pulumi-provider-name
